ef62e38d31fafadc33d3e1860ffad76c7e54c32b
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:48:31 +0700

    deklarasi member variabel pada class Ibu,membuat constructor dan destructor pada class Ibu dan deklarasi prosedur untuk tambahAnak() dan cetakAnak()

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -10,7 +10,23 @@ class anak;
 
 //membuat class Ibu di file header Ibu.h
 class Ibu {
+public:
+    //deklarasi member variabel pada class Ibu
+        string nama;
+        vector<anak*> daftar_anak;
 
+        //membuat constructor dan destructor pada class Ibu
+        Ibu(string pNama) :nama(pNama) {
+            cout << "Ibu \"" << nama << "\" ada\n";
+        };
+
+        ~Ibu() {
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        };
+
+        //deklarasi prosedur untuk tambahAnak() dan cetakAnak()
+        void tambahAnak(anak* pAnak);
+        void cetakAnak();
 };
 
 #endif
