7ae142bfb99f1d95aac3f5367ebb3226680b245c
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:58:40 +0700

    deklarasi class Mahasiswa, membuat member class public, membuat member initialization list dari class mahasiswa dan membuat destructor pada class mahasiswa

--- ParadigmaOOP1.cpp
+++ ParadigmaOOP1.cpp
@@ -8,24 +8,16 @@ class Mahasiswa {
     string nama;
     float nilai;
 
-    //membbuat member class public
+    //membuat member class public
     public:
     //membuat member initialization list dari class mahasiswa
     Mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
         
     };
-    //membuata destructor pada class mahasiswa
+    //membuat destructor pada class mahasiswa
     ~Mahasiswa() {
         cout << "id " << id <<endl;
         cout << "Nama: " << nama << endl;
         cout << "Nilai: " << nilai << endl;
     }
 };
-
-int main()
-{
-    //deklarasi dan pemberian nilai object mhs
-    Mahasiswa mhs(12, "Asroni", 90.5);
-
-    return 0;
-}
