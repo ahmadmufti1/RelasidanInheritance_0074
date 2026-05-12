d468dd1436c7f8b66520a30246c4386a668cc39a
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:52:51 +0700

    pembuatan class dokter, deklarasi member variabel pada class dokter, pembuatan constructor dan destructor pada class dokter, dan deklarasi prosedur untuk tambahPasien() dan cetakPasien()

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -22,3 +22,23 @@ class pasien {
         void tambahDokter(dokter*);
         void cetakDokter();
 };
+
+//pembuatan class dokter
+class dokter{
+    public:
+    //deklarasi member variabel pada class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    //pembuatan constructor dan destructor pada class dokter
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    };
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur untuk tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
