f7c02f3575239905daef34ddce335c705662ed56
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:51:40 +0700

    deklarasi dan pembuatan calss pasien,pembuatan constructor dan destructor pada class pasien,dan deklarasi prosedur untuk tambahDokter() dan cetakDokter()

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -4,3 +4,21 @@ using namespace std;
 
 //deklarasi class dokter
 class dokter;
+//deklarasi dan pembuatan calss pasien
+class pasien {
+    public:
+        string nama;
+        vector<dokter*> daftar_dokter;
+
+        //pembuatan constructor dan destructor pada class pasien
+        pasien(string pNama) : nama(pNama) {
+            cout << "Pasien \"" << nama << "\" ada\n";
+        };
+        ~pasien() {
+            cout << "Pasien \"" << nama << "\" tidak ada\n";
+        };
+
+        //deklarasi prosedur untuk tambahDokter() dan cetakDokter()
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
