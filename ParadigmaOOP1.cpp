c260903e7506a9cb25a8853e77c6914e4408789c
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:59:00 +0700

    membuat program utama

--- ParadigmaOOP1.cpp
+++ ParadigmaOOP1.cpp
@@ -21,3 +21,11 @@ class Mahasiswa {
         cout << "Nilai: " << nilai << endl;
     }
 };
+
+int main()
+{
+    //deklarasi dan pemberian nilai object mhs
+    Mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
+}
