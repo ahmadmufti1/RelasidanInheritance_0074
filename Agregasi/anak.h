bdddccdfbad06151beb319d86bd695813693e8b9
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-12 18:47:00 +0700

    deklarasi member variabel pada class anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -1,9 +1,16 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+#include <iostream>
+using namespace std;
+
 //membuat class anak di file headaer anak.h
 class anak {
+public:
+        //deklarasi member variabel pada class anak
+        string nama;
 
+       
 };
 
 #endif
